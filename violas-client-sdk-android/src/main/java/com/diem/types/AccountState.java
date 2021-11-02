eee9063e68c6e5805e0788d238c3a001921ba446
HunterSun2018 <HunterSun2018@github.com>
2021-11-02 17:57:30 +0800

    update AccountState.java

--- violas-client-sdk-android/src/main/java/com/diem/types/AccountState.java
+++ violas-client-sdk-android/src/main/java/com/diem/types/AccountState.java
@@ -1,8 +1,13 @@
 package com.diem.types;
 
+import com.google.common.reflect.TypeToken;
+import com.novi.bcs.BcsDeserializer;
 import com.novi.serde.Bytes;
+import com.novi.serde.Deserializer;
 import com.novi.serde.Tuple2;
 
+import java.lang.reflect.InvocationTargetException;
+import java.util.Arrays;
 import java.util.Map;
 import java.util.TreeMap;
 
@@ -17,15 +22,36 @@ public class AccountState {
         deserializer.increase_container_depth();
         AccountState.Builder builder = new AccountState.Builder();
 
-        TreeMap<Bytes, Bytes> treeMap = new TreeMap<>();
+        TreeMap<byte[], byte[]> treeMap = new TreeMap<>((bytes1, bytes2) ->  {
+            if(bytes1 == bytes2)
+                return 0;
 
-        long len = deserializer.deserialize_len();
+            int length = bytes1.length;
+            int cmp;
+            if ((cmp = Integer.compare(length, bytes2.length)) != 0) {
+                return cmp;
+            }
+
+            int len = Math.min(bytes1.length, bytes2.length);
+
+            for (int i = 0; i < len; i++) {
+                if(bytes1[i] != bytes2[i])
+                    return bytes1[i] - bytes2[i];
+            }
+
+            return  0;
+        });
+
+        Bytes bytes = deserializer.deserialize_bytes();
+        Deserializer des = new BcsDeserializer(bytes.content());
+
+        long len = des.deserialize_len();
+
         for (int i = 0; i < len; i++) {
             Tuple2<Bytes, Bytes> pair;
 
-            treeMap.put(deserializer.deserialize_bytes(), deserializer.deserialize_bytes());
+            treeMap.put(des.deserialize_bytes().content(), des.deserialize_bytes().content());
         }
-        //builder.value
 
         deserializer.decrease_container_depth();
         return builder.build();
@@ -44,7 +70,7 @@ public class AccountState {
     }
 
     public static final class Builder {
-        public Map<Bytes, Byte> value;
+        public Map<byte[], byte[]> value;
 
         public AccountState build() {
             return new AccountState(
@@ -53,9 +79,8 @@ public class AccountState {
         }
     }
 
-//    public <T extends Object> T getResource(byte[] key) {
-//        T t = new T;
-//
-//        return t;
-//    }
+    public <T extends Object> T getResource(byte[] key, Class<T> object) throws NoSuchMethodException, IllegalAccessException, InvocationTargetException, InstantiationException v{
+
+        return object.getDeclaredConstructor().newInstance();
+    }
 }
---
+++ violas-client-sdk-android/src/test/java/com/diem/ExchangeRate.java
@@ -0,0 +1,11 @@
+package com.diem;
+
+//public class ExchangeRate {
+//    public String currency;
+//    public double  rate;
+//
+//    public ExchangeRate() {
+//        currency = "";
+//        rate = 1;
+//    }
+//}
